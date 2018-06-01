bea62b8ed1dfd2d3f738d343d3cac9afb55415a4
Prachi Gupta <prachiguptage@gmail.com>
2018-06-01 15:12:17 -0700

    Feature: adding click Functionality

--- js/app.js
+++ js/app.js
@@ -36,3 +36,13 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+//enable clicking of card 
+const cardList = document.querySelectorAll('.card');
+
+for(let card of cardList){
+	card.addEventListener('click',function(){
+		card.classList.toggle('open');
+		card.classList.toggle('show');
+	});
+}
+
